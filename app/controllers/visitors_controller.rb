163a82e0fb8ead31ca8d6c017b04dd276f8811db
Safan M <marediasafan@gmail.com>
2018-08-03 11:59:43 -0400

    fix routes and visitor controller

---
+++ app/controllers/visitors_controller.rb
@@ -0,0 +1,28 @@
+class VisitorsController < ApplicationController
+  def index
+    # @visitors = Visitor.all
+  end
+
+  def new
+    @visitor = Visitor.new
+  end
+
+  def show
+    @visitor = Visitor.find(params[:id])
+  end
+
+  def create
+    @visitor = Visitor.new(visitor_params)
+
+    if visitor.save
+      redirect_to @visitor
+    else
+      render 'register'
+    end
+  end
+
+  private
+  def visitor_params
+    params.require(:visitor).permit(:apt_num, :phone, :license_plate, :building)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,11 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get 'visitor/register'
   get 'visitor/reports'
   get 'visitor/settings'
-  get 'buildings/show'
-  get 'buildings/new'
-  get 'buildings/update'
-  get 'buildings/create'
-  get 'buildings/edit'
-  get 'buildings/delete'
-  get 'visitor/index'
-  get 'buildings/index'
-  get 'user/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'user#index'
+  root 'users#index'
 
+  resources :visitors
   resources :buildings
-  resources :user
-  resources :visitor
+  resources :users
 end
